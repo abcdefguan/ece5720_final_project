c31cc17e83ef7eaf14e06a7fa599335eba5b29a3
Guan <gqwu3000@gmail.com>
2020-05-07 16:48:44 +0800

    Prepare for release

--- benchmark.sh
+++ benchmark.sh
@@ -19,20 +19,20 @@ make
 #	./boruvka2 $n 50 >> results/boruvka
 #done
 
-rm results/filter_kruskal2
-for n in 10000 50000 100000 500000 1000000
-do
-for p in 1 2 3 4 5 6 8 10 12 16 24
-do
-	./filter_kruskal $n 50 $p >> results/filter_kruskal2
-done
-done
-
-#rm results/parallel_boruvka
+#rm results/filter_kruskal2
 #for n in 10000 50000 100000 500000 1000000
 #do
 #for p in 1 2 3 4 5 6 8 10 12 16 24
 #do
-#	./parallel_boruvka $n 50 $p >> results/parallel_boruvka
+#	./filter_kruskal $n 50 $p >> results/filter_kruskal2
 #done
 #done
+
+rm results/parallel_boruvka2
+for n in 10000 50000 100000 500000 1000000
+do
+for p in 1 2 3 4 5 6 8 10 12 16 24
+do
+	./parallel_boruvka $n 50 $p >> results/parallel_boruvka2
+done
+done
--- plots/plot.py
+++ plots/plot.py
@@ -2,11 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 n = np.array([10000, 50000, 100000, 500000, 1000000])
-normal_kruskal = np.array([3.91e-02, 2.23e-01, 4.79e-01, 2.66e+00, 5.55e+00])
+
+boruvka = np.array([3.64e-01, 3.03e+00, 6.94e+00, 5.09e+01, 1.15e+02])
 filter_kruskal_one = np.array([1.38e-02, 7.26e-02, 1.50e-01, 9.80e-01, 1.99e+00])
-filter_kruskal_two = np.array([2.60e-02, 1.33e-01, 2.78e-01, 1.51e+00, 2.92e+00])
-filter_kruskal_four = np.array([2.29e-02, 1.19e-01, 2.47e-01, 1.28e+00, 2.49e+00])
+prim = np.array([6.48e-02, 4.33e-01, 8.96e-01, 6.35e+00, 1.45e+01])
+
 filter_kruskal_six = np.array([2.25e-02, 1.13e-01, 2.29e-01, 1.20e+00, 2.31e+00])
+
+#parallel_boruvka_one = np.array([3.63e-01, 3.27e+00, 7.34e+00, 5.27e+01, 1.23e+02])
+#parallel_boruvka_three = np.array([1.41e-01, 1.35e+00, 2.83e+00, 1.91e+01, 4.76e+01])
+#parallel_boruvka_six = np.array([8.95e-02, 8.37e-01, 1.55e+00, 1.19e+01, 2.91e+01])
+parallel_boruvka_twelve = np.array([6.00e-02, 5.67e-01, 1.42e+00, 7.55e+00, 2.13e+01])
+#parallel_boruvka_twentyfour = np.array([6.92e-02, 5.81e-01, 1.15e+00, 8.27e+00, 1.72e+01])
+
+#normal_kruskal = np.array([3.91e-02, 2.23e-01, 4.79e-01, 2.66e+00, 5.55e+00])
+#filter_kruskal_one = np.array([1.38e-02, 7.26e-02, 1.50e-01, 9.80e-01, 1.99e+00])
+#filter_kruskal_two = np.array([2.60e-02, 1.33e-01, 2.78e-01, 1.51e+00, 2.92e+00])
+#filter_kruskal_four = np.array([2.29e-02, 1.19e-01, 2.47e-01, 1.28e+00, 2.49e+00])
+#filter_kruskal_six = np.array([2.25e-02, 1.13e-01, 2.29e-01, 1.20e+00, 2.31e+00])
 
 #normal_kruskal = np.array([3.91e-02, 2.23e-01, 4.79e-01, 2.66e+00, 5.55e+00])
 #filter_kruskal_one = np.array([1.18e-02, 7.75e-02, 1.57e-01, 9.84e-01, 2.12e+00])
@@ -36,12 +49,32 @@ filter_kruskal_six = np.array([2.25e-02, 1.13e-01, 2.29e-01, 1.20e+00, 2.31e+00]
 #plt.yscale('log')
 #plt.show()
 
-plt.title("Runtime vs Number of graph nodes (50 edges per node)")
+plt.title("Speedup vs Number of graph nodes (50 edges per node)")
 plt.xlabel("Number of graph nodes (n)")
 plt.ylabel("Runtime / s")
-runtime = filter_kruskal_one / filter_kruskal_six
-print(runtime)
-x_pos = np.arange(len(runtime))
-plt.bar(x_pos, runtime)
-plt.xticks(x_pos, ['10000', '50000', '100000', '500000', '1000000'])
+
+plt.plot(n, filter_kruskal_one, '-x')
+plt.plot(n, prim, '-x')
+plt.plot(n, boruvka, '-x')
+plt.plot(n, parallel_boruvka_twelve, '-x')
+plt.plot(n, filter_kruskal_six, '-x')
+
+#plt.plot(n, boruvka, '-x')
+#plt.plot(n, parallel_boruvka_one, '-x')
+#plt.plot(n, parallel_boruvka_three, '-x')
+#plt.plot(n, parallel_boruvka_six, '-x')
+#plt.plot(n, parallel_boruvka_twelve, '-x')
+#plt.plot(n, parallel_boruvka_twentyfour, '-x')
+
+plt.legend(['kruskal', 'prim', 'boruvka', 'parallel boruvka', 'filter kruskal'])
+#speedup = boruvka / parallel_boruvka_twelve
+#print(speedup)
+#plt.ylabel("Times speedup")
+#x_pos = np.arange(len(speedup))
+#print(x_pos)
+#plt.bar(x_pos, speedup)
+#plt.xticks(x_pos, ['10000', '50000', '100000', '500000', '1000000'])
+
+plt.xscale('log')
+plt.yscale('log')
 plt.show()
